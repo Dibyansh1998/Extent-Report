93f3095a8522841c05c0ea1e0835ee48971fc8ec
Dibyansh1998 <dibyansh.verma25@gmail.com>
2024-05-11 11:59:52 +0530

    Extent Report class added

---
+++ src/test/java/DibyanshVerma/ExtendReports/ExtentReportDemo.java
@@ -0,0 +1,43 @@
+package DibyanshVerma.ExtendReports;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReportDemo {
+
+	ExtentReports rep;
+
+	@BeforeTest
+	public void config() {
+
+		// ExtentReports, ExtentSparkReporter
+		String path = System.getProperty("user.dir") + "\\reports\\index.html";
+		ExtentSparkReporter report = new ExtentSparkReporter(path);
+		report.config().setReportName("Web Automation Results");
+		report.config().setDocumentTitle("Test Results");
+
+		rep = new ExtentReports();
+		rep.attachReporter(report);
+		rep.setSystemInfo("Automation Tester", "Dibyansh Verma");
+	}
+
+	@Test
+	public void initialDemo() {
+
+		ExtentTest test=rep.createTest("InitialDemo");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com");
+		System.out.println(driver.getTitle());
+		driver.close();
+		test.fail("Result don't match");
+		
+		rep.flush();
+	}
+
+}
